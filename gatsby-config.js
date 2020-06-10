97a9b6bf91ccd309c0eebb5223c726a243f4714a
Robert Pirtle <astropirtle@gmail.com>
2020-06-09 20:32:05 -0700

    make it my own

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Laniakean Explorer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@pirtleshell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
